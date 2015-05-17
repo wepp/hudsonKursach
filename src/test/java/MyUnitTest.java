23aa732089e1ea8e47708f49e25f6b57a5aeccbe
Artem <sileage2@gmail.com>
2015-05-17 16:57:59 +0300

    22

--- src/test/java/MyUnitTest.java
+++ src/test/java/MyUnitTest.java
@@ -4,12 +4,20 @@ import static junit.framework.Assert.assertEquals;
 public class MyUnitTest {
 
     @Test
-    public void testConcatenate() {
+     public void testConcatenate() {
         MyUnit myUnit = new MyUnit();
 
         String result = myUnit.concatenate("one", "two");
         int a = myUnit.calA(3, 4);
         assertEquals("onetwo", result);
+        assertEquals(7,a);
+
+    }
+
+    @Test
+    public void testCalc() {
+        MyUnit myUnit = new MyUnit();
+        int a = myUnit.calA(3, 4);
         assertEquals(7,a);
 
     }
